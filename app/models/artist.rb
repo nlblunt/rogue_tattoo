1bb840b683e273085f0124ac9dceb0d99353079b
nlblunt <nicolas.blunt@gmail.com>
2014-06-22 11:52:14 -0700

    Update missing image path

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,7 @@ class Artist < ActiveRecord::Base
 	
 	accepts_nested_attributes_for :images
 	
-    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.jpg"
+    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path(':style/missing.jpg') #"/images/:style/missing.jpg"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/ #["image/jpg", "image/jpeg", "image/png"]
   
   attr_accessible :name, :bio, :avatar
