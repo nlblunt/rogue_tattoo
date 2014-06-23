fb5a5f3f6b0023090f64f74eeb9ae3147950b172
nlblunt <nicolas.blunt@gmail.com>
2014-06-22 17:09:51 -0700

    Update missing image path

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
 	belongs_to :story
 	belongs_to :artist
 	
-    has_attached_file :img, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => 'https://s3-us-west-2.amazonaws.com/rogue-tattoo/common/missing_:style.jpg' 
+    has_attached_file :img, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/:style/missing.jpg"
 
 
     validates_attachment_content_type :img, :content_type => /\Aimage\/.*\Z/ #["image/jpg", "image/jpeg", "image/png"]
