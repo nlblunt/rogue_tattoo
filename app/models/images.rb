825fc3db6ac712b3d8d641b49aaa9f6981e9b5a7
nlblunt <nicolas.blunt@gmail.com>
2014-07-03 23:08:37 -0700

    Removed duplicate model file

--- app/models/images.rb
+++
@@ -1,9 +0,0 @@
-class Images < ActiveRecord::Base
-	belongs_to :story
-	
-    has_attached_file :img, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-    
-    validates_attachment_content_type :img, :content_type => ["image/jpg", "image/jpeg", "image/png"]
-        
-    attr_accessible :img, :story_id
-end
