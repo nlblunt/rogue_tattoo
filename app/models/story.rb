4627baa31a71de957cecd955d6250f89897029c4
nlblunt <nicolas.blunt@gmail.com>
2014-07-22 15:19:03 -0700

    Removed old files

--- app/models/story.rb
+++
@@ -1,8 +0,0 @@
-#Legacy class.  Saving incase adding back in
-class Story < ActiveRecord::Base
-			belongs_to :client
-			has_many :images, dependent: :destroy
-			
-			accepts_nested_attributes_for :images
-    #attr_accessible :title, :description, :date, :client_id, :artist_id, :featured
-end
