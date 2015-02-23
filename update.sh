8ae32362e68d560ac3862e495c2f6b75fb47b503
Nicolas <nicolas.blunt@gmail.com>
2015-02-23 10:35:32 -0800

    Testing

--- update.sh
+++ update.sh
@@ -9,6 +9,8 @@ echo "Resetting permission for public folder"
 sudo chown -R daemon:daemon public
 echo "Resetting permission for tmp folder"
 sudo chown -R daemon:daemon tmp
+echo "Resetting permission for logs"
+sudo chown -R daemon:deamon log
 echo "Updating completed"
 sudo touch /tmp/restart.txt
 echo "Phusion Passenger restarted"
