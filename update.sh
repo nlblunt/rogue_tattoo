dc7106c98dbe4adaab3adba523095c49d39ad505
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2015-08-27 16:22:38 +0000

    Figaro gem

--- update.sh
+++ update.sh
@@ -3,6 +3,12 @@ clear
 echo "Updating working files"
 git pull origin master
 
+echo "Building Gemfile"
+bundle install
+
+echo "Checking for migrations"
+rake db:migrate RAILS_ENV=production
+
 echo "Rebuilding assets"
 rake assets:clobber
 rake assets:precompile
@@ -10,4 +16,4 @@ rake assets:precompile
 echo "Updating completed"
 
 echo "Restarting Passenger"
-touch /tmp/restart.txt
+sudo touch /tmp/restart.txt
